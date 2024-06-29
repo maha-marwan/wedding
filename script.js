87b1d465e9c933111ffc0281ea14a3384e80a15b
Ruqayyah <rmuse1@student.gsu.edu>
2024-06-29 13:00:04 -0400

    Update venue and reception details

--- script.js
+++ script.js
@@ -86,30 +86,50 @@ function mobileView() {
         //     container.children[1].style.marginBottom = "4vh";
         // }
 
+        // reception_date
+        var reception_date = document.getElementsByClassName("reception_date")[0];
+        reception_date.style.fontSize = "6.5vw";
+        reception_date.style.padding = "3vh 8vw 0 8vw";
+
         // venue section
         var venue = document.getElementsByClassName("venue")[0];
         venue.style.flexDirection = "column";
-        venue.style.padding = "3vh 8vw";
+        venue.style.paddingLeft = "8vw";
+        venue.style.paddingRight = "8vw";
 
         // venue address and date
-        var venue_info = venue.children[0];
-        venue_info.style.width = "100%";
-        venue_info.style.marginBottom = "2vh";
-        venue_info.children[0].style.fontSize = "6.5vw";
-        venue_info.children[0].innerText = "Premier Event Halls";
-        venue_info.children[1].style.fontSize = "4.5vw";
-        venue_info.children[2].style.fontSize = "4.5vw";
-        venue_info.children[2].innerHTML = "<span style='font-weight:600;'>Date:</span> 12.29.2024 | 2 pm - 7 pm";
+        for (let j = 0; j < venue.children.length; j++) {
+            const location = venue.children[j];
+            location.style.width = "auto";
+            location.children[0].style.fontSize = "4.5vw";
+            location.children[1].style.fontSize = "6.5vw";
+            location.children[2].style.fontSize = "4.5vw";
+        }
+
+        venue.children[0].children[2].innerHTML = "<span class='bold'>Address:</span> 3520 Breckinridge Blvd<br>Duluth, Georgia 30096";
+        venue.children[1].children[2].innerHTML = "<span class='bold'>Address:</span> 3570 Breckinridge Blvd<br>Duluth, Georgia 30096";
+        venue.children[1].style.marginTop = "1.25vh";
+        // var venue_info = venue.children[0];
+        // venue_info.style.width = "100%";
+        // venue_info.style.marginBottom = "2vh";
+        // venue_info.children[0].style.fontSize = "6.5vw";
+        // venue_info.children[0].innerText = "Premier Event Halls";
+        // venue_info.children[1].style.fontSize = "4.5vw";
+        // venue_info.children[2].style.fontSize = "4.5vw";
+        // venue_info.children[2].innerHTML = "<span style='font-weight:600;'>Date:</span> 12.29.2024 | 2 pm - 7 pm";
 
         // reception details
-        var reception_details = venue.children[1];
-        reception_details.style.width = "100%";
+        var reception_details = document.getElementsByClassName("container_rules")[0];
+        reception_details.style.padding = "1.5vh 8vw 3vh 8vw";
+
         reception_details.children[0].style.fontSize = "6vw";
 
         reception_list = reception_details.children[1].children;
         for (let j = 0; j < reception_list.length; j++) {
             const li = reception_list[j];
             li.style.fontSize = "4vw";
+            li.style.paddingTop = "0.5vh";
+            li.style.paddingBottom = "0.5vh";
         }
 
         // rsvp section
@@ -117,9 +137,9 @@ function mobileView() {
         rsvp.style.padding = "3vh 8vw";
 
         rsvp.children[0].style.fontSize = "6.5vw";
-        var rsvp_button = rsvp.children[1].children[0];
-        rsvp_button.style.fontSize = "4.5vw";
-        rsvp_button.style.padding = "2.5vh 15vw";
+        // var rsvp_button = rsvp.children[1].children[0];
+        // rsvp_button.style.fontSize = "4.5vw";
+        // rsvp_button.style.padding = "2.5vh 15vw";
         
         // footer
         document.getElementsByClassName("footer")[0].style.fontSize = "2vw";
